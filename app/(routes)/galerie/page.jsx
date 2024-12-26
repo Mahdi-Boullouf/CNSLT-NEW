3f3c8bd343f3c4950cc9c4378c95465b17e40f26
BOUDAOUD ZAKARIA <107261934+boudaoud-zakaria@users.noreply.github.com>
2024-12-26 22:57:41 +0100

    Update page.jsx

--- app/(routes)/galerie/page.jsx
+++ app/(routes)/galerie/page.jsx
@@ -9,7 +9,7 @@ const tikejdaAreas = [
   'Chalet de Thighzart',
   'Hôtel Djurdjura',
   'Hôtel Tikjda',
-  'Hotel Pinot Noir'  // Added this area
+  'Hotel Pin Noir'  // Added this area
 ].map(name => name.toLowerCase());
 
 export default function Galerie() {
@@ -71,7 +71,7 @@ export default function Galerie() {
             }}
           />
           {/* Area options div */}
-          {areaOptions.map((area) => area != 'Chalet de Thighzart' && area != 'Hôtel Djurdjura' && area != 'Hôtel Tikjda' && area != 'Hotel Pinot Noir' ? (
+          {areaOptions.map((area) => area != 'Chalet de Thighzart' && area != 'Hôtel Djurdjura' && area != 'Hôtel Tikjda' && area != 'Hotel Pin Noir' ? (
             <div
               key={area}
               className={`flex-1 px-4 font-bold text-primary2 font-body uppercase text-lg sm:text-sm flex items-center justify-center cursor-pointer relative z-10 text-center transition-colors duration-300 ${
