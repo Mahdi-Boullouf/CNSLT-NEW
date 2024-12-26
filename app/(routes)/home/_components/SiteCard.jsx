c0f018efa0924606c2702ff1db2bdea9e41d268c
BOUDAOUD ZAKARIA <107261934+boudaoud-zakaria@users.noreply.github.com>
2024-12-26 23:02:31 +0100

    Update SiteCard.jsx

--- app/(routes)/home/_components/SiteCard.jsx
+++ app/(routes)/home/_components/SiteCard.jsx
@@ -18,7 +18,7 @@ const SiteCard = ({ _id,images, name, duration, transport, type, rating, reviews
       />
       <div className="px-6 py-4">
       <h2 className="font-bold text-xl mb-2 line-clamp-2 h-14 overflow-hidden">
-          {name}
+          {name} 
         </h2>        {/* <div className="flex items-center mb-2">
           <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
